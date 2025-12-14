43afb6664b42a59f1dd84ce852c1295cba86b3f9
zzzz <zz@zzzzui-MacBookAir.local>
2025-12-15 05:07:11 +0900

    lol-oop2

---
+++ src/Ezreal.java
@@ -0,0 +1,11 @@
+public final class Garen extends Champion {
+    public Garen() {
+        super("isreal", GameConstants.DEFAULT_HP, GameConstants.DEFAULT_ATK, GameConstants.DEFAULT_DEF);
+    }
+    {}
+
+    @Override
+    public void useQ(Champion target) {
+        // Q 스킬 구현
+    }
+}
